490d015b5d676dc814ee53b3478c27d9455d81b0
samekh_resh <C0d1ng4Lyf3>
2022-06-02 18:11:18 -0400

    linked lists, binary search algorithm, quick sort

--- Lab2.6_Stack_Starter/src/com/algorithms/Main.java
+++ Lab2.6_Stack_Starter/src/com/algorithms/Main.java
@@ -2,4 +2,13 @@ package com.algorithms;
 
 class Main {
 
+    public static void main(String[] args) {
+        Stack<Customer> customerStack= new Stack<>();
+
+        customerStack.push(new Customer(22, "mark", 22f));
+        customerStack.push(new Customer(32, "jacob", 21f));
+        customerStack.push(new Customer(542, "mark", 12f));
+        Customer custom = customerStack.pop();
+        System.out.println(custom.toString());
+    }
 }
